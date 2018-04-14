ab37282319c24baee1b8135c5ef9f5c5e39ee22a
Nasirou Wagana <wnasirou@gmail.com>
2018-04-14 13:30:01 +0100

    Creating a Post Model, Factory and Controller

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'Helllo World';
+});
+
+Route::get('/users/{id}', 'UserController@show')->name('users.show');
+Route::get('/posts', 'PostController@index')->name('posts.show');
